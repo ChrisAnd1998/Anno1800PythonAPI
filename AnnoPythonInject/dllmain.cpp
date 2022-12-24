c986935c0647241d6d70cd39748df94e40181c40
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2022-12-24 20:43:15 +0100

    Add files via upload

--- AnnoPythonInject/dllmain.cpp
+++ AnnoPythonInject/dllmain.cpp
@@ -2,33 +2,27 @@
 #include <string>
 #include <tlhelp32.h>
 
+
+HINSTANCE selfModuleHandle;
+bool unload = false;
+
+typedef int(__stdcall* PY_ISINITIALIZED)();
+typedef int(__stdcall* PYRUN_SIMPLESTRING)(const char* str);
+typedef DWORD* (__stdcall* PYGILSTATE_ENSURE)();
+typedef void(__stdcall* PYGILSTATE_RELEASE)(DWORD* gstate);
+
+HMODULE hModule = GetModuleHandle(L"python35.dll");
+PY_ISINITIALIZED Py_IsInitialized = (PY_ISINITIALIZED)GetProcAddress(hModule, "Py_IsInitialized");
+PYGILSTATE_ENSURE PyGILState_Ensure = (PYGILSTATE_ENSURE)GetProcAddress(hModule, "PyGILState_Ensure");
+PYGILSTATE_RELEASE PyGILState_Release = (PYGILSTATE_RELEASE)GetProcAddress(hModule, "PyGILState_Release");
+PYRUN_SIMPLESTRING PyRun_SimpleString = (PYRUN_SIMPLESTRING)GetProcAddress(hModule, "PyRun_SimpleString");
+
 BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
         _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
-
-        HMODULE hModule;
-        HINSTANCE selfModuleHandle;
-        bool unload = false;
-
         selfModuleHandle = hInst;
-
-        typedef int(__stdcall* PY_ISINITIALIZED)();
-        typedef int(__stdcall* PYRUN_SIMPLESTRING)(const char* str);
-        typedef DWORD* (__stdcall* PYGILSTATE_ENSURE)();
-        typedef void(__stdcall* PYGILSTATE_RELEASE)(DWORD* gstate);
-
-        PY_ISINITIALIZED Py_IsInitialized;
-        PYRUN_SIMPLESTRING PyRun_SimpleString;
-        PYGILSTATE_ENSURE PyGILState_Ensure;
-        PYGILSTATE_RELEASE PyGILState_Release;
-
-        hModule = GetModuleHandle(L"python35.dll");
-        Py_IsInitialized = (PY_ISINITIALIZED)GetProcAddress(hModule, "Py_IsInitialized");
-        PyGILState_Ensure = (PYGILSTATE_ENSURE)GetProcAddress(hModule, "PyGILState_Ensure");
-        PyGILState_Release = (PYGILSTATE_RELEASE)GetProcAddress(hModule, "PyGILState_Release");
-        PyRun_SimpleString = (PYRUN_SIMPLESTRING)GetProcAddress(hModule, "PyRun_SimpleString");
 
         if (Py_IsInitialized != 0)
         {
@@ -52,16 +46,21 @@ BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD ul_reason_for_call, LPVOID lpReserv
             PyGILState_Release(gstate);
         }
 
+      
+
         free(Py_IsInitialized);
         free(PyGILState_Ensure);
         free(PyGILState_Release);
         free(PyRun_SimpleString);
+
 
         bool b = true;
         while (b == true)
         {
             b = FreeLibrary(selfModuleHandle);
         }
+
+        
     }
     return TRUE;
 }
